a1521304e7e237a48dc896d3bd902a30706b8f45
lanny lopez <lannylynnlopez89@gmail.com>
2022-02-27 12:26:03 -0700

    models filled out

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,7 @@
 // import important parts of sequelize library
+const { STRING } = require('mysql2/lib/constants/types');
 const { Model, DataTypes } = require('sequelize');
+const { all } = require('sequelize/types/lib/operators');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +12,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL(6,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      },
+      stock:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate:{ 
+          isNumeric: true
+        }  
+      },
+      category_id:{
+        type: DataTypes.INTEGER,
+        reference:{
+          Model: 'category',
+          key: id
+        }
+      }
+    }
   },
   {
     sequelize,
